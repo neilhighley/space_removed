408aa819be425c36312e1d89407aaa9e5b8dd55d
neil highley <neil@neilhighley.com>
2015-06-27 12:58:26 +0100

    initial api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ranges', function(req, res) {
+  var q={time:new Date(),ranges:[{position:1,active:false},
+    {position:2,active:false},
+    {position:3,active:false},
+    {position:4,active:false},
+    {position:5,active:false},
+    {position:6,active:true},
+    {position:7,active:false},
+    {position:8,active:false},
+    {position:9,active:false}]};
+
+  res.json(q);
+});
+
+
 module.exports = router;
